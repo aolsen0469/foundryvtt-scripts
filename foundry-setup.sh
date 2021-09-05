c0447394eb9d7df84a5eabe832e2668899dc413d
Adam Olsen <aolsen0469@gmail.com>
2021-09-05 13:23:50 -0400

    Update foundry-setup.sh

--- foundry-setup.sh
+++ foundry-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+URL_PATH='https://foundryvtt.s3.amazonaws.com/releases/0.8.9/foundryvtt-0.8.9.zip?AWSAccessKeyId=AKIA2KJE5YZ3EFLQJT6N&Signature=eqOD5zm%2FdtMYbiOUc2P0BVAPUCg%3D&Expires=1630862840'
 APP_PATH='/foundry-app'
 DATA_PATH='/foundry-data'
 mkdir -p ${DATA_PATH}
@@ -6,7 +7,7 @@ mkdir -p ${APP_PATH}
 sudo apt install -y libssl-dev zip
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt install -y nodejs
-wget https://foundryvtt.s3-us-west-2.amazonaws.com/releases/043-8vcvlk129cs7/foundryvtt-0.4.3.zip -O foundryvtt.zip
+wget $URL_PATH -O foundryvtt.zip
 sudo unzip foundryvtt.zip -d ${APP_PATH}
 sudo node ${APP_PATH}/resources/app/main.js --port=80 --dataPath=${DATA_PATH}
 cd ${DATA_PATH}/Data/systems  ; git clone https://gitlab.com/foundrynet/dnd5e
